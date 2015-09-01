721ceb52d0a28161566f1b28de899d8f784bfe79
Peter <NA>
2015-09-01 18:15:16 +1000

    Started Parts Edit Form

---
+++ OPEAForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OPEAManager
+{
+    public partial class OPEAForm : Form
+    {
+        public OPEAForm() {
+            InitializeComponent();
+        }
+    }
+}
