50c380ed2254ca6c2331829ed4fea18bcf4f191e
Peter <NA>
2015-09-22 10:22:33 +1000

    Splash start

---
+++ splash.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace OPEAManager
+{
+    partial class splash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.splashtimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::OPEAManager.Properties.Resources.splash;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(576, 323);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.UseWaitCursor = true;
+            // 
+            // splashtimer
+            // 
+            this.splashtimer.Tick += new System.EventHandler(this.splashtimer_Tick);
+            // 
+            // splash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 323);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MinimizeBox = false;
+            this.Name = "splash";
+            this.Opacity = 0.95D;
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TopMost = true;
+            this.UseWaitCursor = true;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer splashtimer;
+    }
+}
---
+++ splash.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using log4net;
+using log4net.Config;
+
+namespace OPEAManager
+{
+    public partial class splash : Form
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(splash));
+        int count = 0;
+        public splash() {
+            //XmlConfigurator.Configure(new System.IO.FileInfo(@"log4net.xml"));
+            log.Debug("Splash Create");
+            InitializeComponent();
+            Thread.Sleep(1000);
+            splashtimer.Start();
+
+        }
+
+        private void splashtimer_Tick(object sender, EventArgs e) {
+            if (count > 30) {
+                log.Debug("Splash Done");
+                splashtimer.Stop();
+                this.Close();
+            }
+            if (count > 15) {
+                double t = ((15.0 / (count-15)) );
+                this.Opacity = t;
+                this.Refresh();
+                log.Debug(t);
+            }
+            count++;
+        }
+    }
+}
