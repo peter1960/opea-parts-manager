8860171f74c4fcc427613b1e5080e6cbe096656e
Peter <NA>
2015-09-24 23:30:26 +1000

    Error codes

---
+++ stErrors.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPEAManager
+{
+    public enum stErr
+    {
+        OK,                    // all ok
+        FRANCHISE1,            // franchise not defined
+        Supplier
+    }
+
+}
